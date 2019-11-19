46648b491e69de15c4bc64d9fb58e5a6e42d56e0
Bernhard Mallinger <b.mallinger@gmx.at>
2019-11-19 21:03:50 +0100

    Basic implementation of categorer

--- categorer/src/Categorer.hs
+++ categorer/src/Categorer.hs
@@ -7,7 +7,14 @@ module Categorer
     )
 where
 
+import           Control.Lens
+
 import           Options.Generic
+import           Data.Aeson.Lens                ( _String
+                                                , key
+                                                , values
+                                                )
+import qualified Network.Wreq                  as WREQ
 
 data WordCategoryOptions = WCO
     { word :: Text <?> "Search for categories of this word"
@@ -16,7 +23,29 @@ data WordCategoryOptions = WCO
 
 instance ParseRecord WordCategoryOptions
 
+constructUrl :: Text -> Text
+constructUrl leWord =
+    "https://en.wiktionary.org/w/api.php?action=parse&page="
+        <> leWord
+        <> "&prop=categories&format=json"
+
+retrieveCategories :: Text -> IO [Text]
+retrieveCategories leWord = do
+    response <- WREQ.get $ unpack $ constructUrl leWord
+    return
+        $   response
+        ^.. WREQ.responseBody
+        .   key "parse"
+        .   key "categories"
+        .   values
+        .   key "*"
+        .   _String
+
 appMain :: IO ()
 appMain = do
     wco <- getRecord "Categorer"
-    print (wco :: WordCategoryOptions)
+    let leWord = unHelpful $ word (wco :: WordCategoryOptions)
+    categories <- retrieveCategories leWord
+
+    putStrLn $ "Categories of " <> leWord <> ":"
+    mapM_ putStrLn categories
