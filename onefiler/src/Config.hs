d2a61d174ce44a8dd139103d117ba7fcf1d6ebfe
Bernhard Mallinger <b.mallinger@gmx.at>
2019-12-03 16:30:39 +0100

    Play around with multiple config locations monoid

--- onefiler/src/Config.hs
+++ onefiler/src/Config.hs
@@ -2,15 +2,47 @@ module Config
     ()
 where
 
-import           ClassyPrelude
+import           System.Directory
 
-possiblePaths :: [Text]
-possiblePaths = ["/etc/myconf.conf", "~/.myconf.conf", "~/.config/myconf.conf"]
+possiblePaths :: IO [Text]
+possiblePaths = do
+    home <- getHomeDirectory
+    pure
+        [ "/etc/myconf.conf"
+        , pack home <> "/.myconf.conf"
+        , pack home <> "/.config/myconf.conf"
+        ]
 
-
-newtype Config = Config (Maybe Int)
+newtype Config = Config (Maybe Int) deriving (Show)
 
 instance Semigroup Config where
-    (Config _      ) <> (Config (Just c)) = Config (Just c)
-    (Config Nothing) <> d                 = d
---instance Monoid Config
+    _ <> (Config (Just c)) = Config (Just c)
+    c <> _                 = c
+
+
+instance Monoid Config where
+    mempty = Config Nothing
+
+
+getActualConfig1 :: IO Config
+getActualConfig1 = do
+    possiblePaths' <- possiblePaths
+    fileContents   <- sequence (readFileUtf8 . show <$> possiblePaths')
+    let possibleConfigs :: [Maybe Int] = readMay <$> fileContents
+    let configs                        = Config <$> possibleConfigs
+    pure $ mconcat configs
+
+
+getActualConfig2 :: IO Config
+getActualConfig2 = do
+    possiblePaths' <- possiblePaths
+    mconcat <$> sequence (parseConfig <$> possiblePaths')
+  where
+    parseConfig :: Text -> IO Config
+    parseConfig path =
+        Config
+            .   readMay
+            <$> (ignoreError (traceShowId <$> (readFileUtf8 $ show path)))
+
+    ignoreError :: Monoid a => IO a -> IO a
+    ignoreError io = io `catch` (\(_ :: IOError) -> pure mempty)
